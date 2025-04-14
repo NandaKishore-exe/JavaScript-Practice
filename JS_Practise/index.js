e4746bc0b3d1c9e52e41dd56f0b138df46d7f90c
NandaKishore-exe <nandakishorelogu@gmail.com>
2025-04-14 21:20:13 +0530

    completed till event handling

--- JS_Practise/index.js
+++ JS_Practise/index.js
@@ -83,5 +83,133 @@ animal.color = "Yellow";
 console.log(animal.name);
 console.log(person["firstName"]);
 
-//
+//Object Methods
+//Methods are actions that can be performed on objects. Methods are function definitions stored as property values.
+
+const car2 = {
+  name:"Hyundai",
+  model: "i20",
+  engine: 1200,
+  start: function start(){
+    console.log(`${this.name} car is started`);//this keyword refers to car2 object, so this.name means the name property of car2 object
+  }
+}
+
+car2.start();
+
+/* JavaScript defines 7 types of primitive data types:
+
+string
+number
+boolean
+null
+undefined
+symbol
+bigint
+
+*/
+
+//JS Objects are Mutable, They are addressed by reference, not by value.
+
+// Z is not a copy of X, both X and Z are same memory address. So any changes to Z will also affect X object.
+
+const x = {name:"dummy", age:12};
+
+const z = x;
+
+z.name = "vibe"
+
+console.log(x.name);
+
+console.log(z.name)
+
+//Adding new properties to object
+
+z.role = "Sofware Engineer";
+
+//Deleting property from an object
+
+delete z.age;
+
+console.log(z);
+
+
+//Nested Objects
+
+//Property values in an object can be other objects
+
+myObj = {
+  name:"John",
+  age:30,
+  myCars: {
+    car1:"Ford",
+    car2:"BMW",
+    car3:"Fiat"
+  }
+}
+
+console.log(myObj.myCars.car2); //Accessing nested objects
+
+//JS Object Display
+
+//Using Object.Values()
+
+console.log(Object.values(car2)); //return values of properties in the object in array format
+
+//Using Object.entries() 
+
+const person2 = {name:"nk", age:25};
+
+for (let[key, value] of Object.entries(person2)){
+  //Oject.entries It converts an object into an array of key-value pairs ["name","nk"], ["age", "25"]
+  console.log(`${key} -> ${value}`);
+}
+
+//Using JSON.stringify()
+
+const person3 = {
+  name: "john",
+  age: 30,
+  city: "New york"
+};
+
+//Stringify Object
+
+let myString = JSON.stringify(person3);
+
+console.log(myString);
+
+//JS Object Constructors
+
+//Below example is constructor Function
+
+function Human (name, age){
+  this.name = name;
+  this.age = age;
+}
+
+const Men = new Human ("john", 30);
+console.log(Men);
+
+//Adding Method to an object
+
+Men.walk = function walk(){
+  console.log("I can Walk");
+}
+
+console.log(Men);
+
+
+//Adding method to constructor function
+
+Human.prototype.run = function run () {
+  console.log("I'm running");
+}
+
+Men.run();
+
+//Event Handling in JS see in html file
+
+
+
 
